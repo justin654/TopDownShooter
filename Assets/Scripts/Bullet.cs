c95ba4585fa60d165437b8dbae567a9074ba2e86
justin654 <justin654@gmail.com>
2023-10-18 01:16:52 -0400

    Weapon Icon
    Updated UI to show current weapons Icon, will need to make some or find some somewhere.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,16 @@ public class Bullet : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float bulletForce; // The force will be set from the weapon when the bullet is instantiated
+    public float lifetime = 2f; // Bullet lifetime in seconds
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         // Apply force to the bullet
         rb.AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
+
+        // Destroy the bullet after a set time.
+        Destroy(gameObject, lifetime);
     }
 
     // Optionally, handle collisions with targets, etc.
--- Assets/Scripts/WeaponUIManager.cs
+++ Assets/Scripts/WeaponUIManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro; // Make sure you have this namespace for TextMeshPro access
+using UnityEngine.UI; // Required for UI elements like Image
+
 
 public class WeaponUIManager : MonoBehaviour
 {
@@ -8,6 +10,8 @@ public class WeaponUIManager : MonoBehaviour
 
     // Reference to your player (or the script/component that holds the current weapon info)
     public GunController playerGunController;
+
+    public Image weaponIcon;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,13 @@ public class WeaponUIManager : MonoBehaviour
             Debug.LogError("Don't forget to assign the player's GunController to this script.");
             this.enabled = false; // Disable this script if the playerGunController is not assigned
         }
+
+        if (weaponIcon == null)
+        {
+            Debug.LogError("Don't forget to assign the Image component to the weaponIcon variable.");
+            this.enabled = false; // Disable this script if the weaponIcon is not assigned
+        }
+
     }
 
     // Update is called once per frame
@@ -30,12 +41,18 @@ public class WeaponUIManager : MonoBehaviour
     {
         if (playerGunController.currentWeapon != null)
         {
-            // Assuming the weapon has a 'name' field. Replace 'name' with the actual field if it's different
+            // Update the text
             weaponText.text = "Current Weapon: " + playerGunController.currentWeapon.name;
+
+            // Update the icon
+            weaponIcon.sprite = playerGunController.currentWeapon.icon; // This assigns the weapon's icon to the Image component
+            weaponIcon.enabled = true; // Ensure the icon is visible
         }
         else
         {
             weaponText.text = "No Weapon Selected";
+            weaponIcon.enabled = false; // Hide the icon if there's no weapon
         }
     }
+
 }
